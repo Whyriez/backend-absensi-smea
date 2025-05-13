249dbd3827bbfdcc9a238cbb8b23cb01c88e3588
Alim Suma <sumaalim@gmail.com>
2025-05-13 17:32:35 +0800

    updated

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
+import express from 'express';
 import createHandler from './users/create.js';
 import deleteHandler from './users/delete.js';
+import updateEmail from './users/updateEmail.js';
+import getEmailHandler from './users/getEmail.js';
 import resetPasswordHandler from './users/resetPassword.js';
 
-export default async function handler(req, res) {
-  switch (req.method) {
-    case 'POST':
-      return createHandler(req, res);
-    case 'DELETE':
-      return deleteHandler(req, res);
-    case 'PUT':
-      return resetPasswordHandler(req, res);
-    default:
-      res.setHeader('Allow', ['POST', 'DELETE', 'PUT']);
-      return res.status(405).json({ message: 'Method Not Allowed' });
-  }
-}
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware untuk parsing JSON request
+app.use(express.json());
+
+app.post('/api/users', createHandler);
+
+app.delete('/api/deleteSiswa', deleteHandler);
+
+app.put('/api/resetPassword', resetPasswordHandler);
+
+app.put('/api/updateEmail', updateEmail);
+
+app.get('/api/getEmail', getEmailHandler);
+
+app.use((req, res) => {
+  res.status(405).json({ message: 'Method Not Allowed' });
+});
+
+// Menjalankan server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
